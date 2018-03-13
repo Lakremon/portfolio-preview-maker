f17a28c11d7f3ce4ba5ad987e4a5fd874817b5b9
Lakremon <teapot_89@mail.ru>
2018-03-14 02:58:46 +0700

    set one second weight duration for the page.

--- src/uploader.ts
+++ src/uploader.ts
@@ -10,6 +10,8 @@ page.viewportSize = {width: winth, height: height};
 //the clipRect is the portion of the page you are taking a screenshot of
 page.clipRect = {top: 0, left: 0, width: winth, height: height};
 page.open(address, function (status) {
-    page.render(imageName);
-    phantom.exit();
+    setTimeout(function(){
+        page.render(imageName);
+        phantom.exit();
+    },1000);
 });
